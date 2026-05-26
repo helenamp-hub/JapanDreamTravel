979bfd712b032cd45193be00c06aba096816a151
helenamp-hub <hmedinaperez01@gmail.com>
2026-05-26 17:42:33 +0200

    Update noticias.js

--- js/noticias.js
+++ js/noticias.js
@@ -1,1 +1,17 @@
 //CARGAR NOTICIAS EXTERNAS, USAR AJAX Y LEER JSON
+[
+    {
+        "titulo": "Festival Sakura 2026",
+        "texto": "Descubre Japón durante la temporada de cerezos."
+    },
+
+    {
+        "titulo": "Nuevas rutas anime",
+        "texto": "Visita Akihabara y los lugares más famosos del anime."
+    },
+
+    {
+        "titulo": "Viajes gastronómicos",
+        "texto": "Explora la cocina japonesa tradicional."
+    }
+]
