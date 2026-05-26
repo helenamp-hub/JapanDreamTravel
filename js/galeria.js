3866aa6314fc292c89e903a2677aaa2d3f219639
helenamp-hub <hmedinaperez01@gmail.com>
2026-05-26 17:53:59 +0200

    Update galeria.js

--- js/galeria.js
+++ js/galeria.js
@@ -1,1 +1,11 @@
+// GALERÍA SIMPLE
 
+const images = document.querySelectorAll(".gallery img");
+
+images.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        image.classList.toggle("active-image");
+    });
+});
